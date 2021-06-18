e506b1c31ade06eb4f9e7312ffa55c508d92e587
rscherrer <raphael.scherrer@evobio.eu>
2021-06-18 11:47:14 +0200

    stuff

--- script.R
+++ script.R
@@ -1,39 +1,80 @@
 rm(list = ls())
 
+library(pracma)
 library(tidyverse)
 
-get_lambda <- function(x, xhat) {
+theme_set(theme_classic())
 
-  e1 <- exp(-s * (x + psi)^2 / psi^2)
-  e2 <- exp(-s * (x - psi)^2 / psi^2)
-  e1hat <- exp(-s * (xhat + psi)^2 / psi^2)
-  e2hat <- exp(-s * (xhat - psi)^2 / psi^2)
-  h11 <- h22 <- 1
-  h12 <- h21 <- h
+source("functions.R")
+source("tests.R")
 
-  N1 == f(N1, N2)
-  N2 == g(N1, N2)
+#### Analysis ####
 
-  R11 <- iota * h11 / (omicron + N1 * e1hat)
-  R12 <- iota * h12 / (omicron + N2 * e1hat)
-  R21 <- iota * h21 / (omicron + N1 * e2hat)
-  R22 <- iota * h22 / (omicron + N2 * e2hat)
-  W1 <- e1 * R11 + e2 * R21
-  W2 <- e1 * R12 + e2 * R22
-  r1 <- 1 - d + 0.5 * W1 * (1 + A)
-  r2 <- 1 - d + 0.5 * W2 * (1 + A)
-  lambda <- 0.5 * ((1 - m) * (r1 + r2) + sqrt((1 - m)^2 * (r1^2 - 2 * r1 * r2 + r2^2) + 4 * m^2 * r1 * r2))
+# Parameter values
+pars <- get_example_pars()
 
-}
+# Approximate rates of evolution
+mu <- 0.01 # mutation rate per locus * number of ecological loci
+sigma <- 0.01 # 1 / number of ecological loci (variance in normalized effect sizes)
 
-FUN <- function() {
+tic <- Sys.time()
 
-  pip <- expand_grid(xhat = seq(-5, 5, 1), x = seq(-5, 5, 1))
+# Simulate evolution
+data <- simulate(0, ntimes = 100000, pars, init = rep(1000, 4), burnin = 20000, mu = mu, sigma = sigma)
 
-  pip <- pip %>% mutate(lambda = get_lambda(x, xhat))
+toc <- Sys.time()
 
-  return(pip)
+toc - tic
 
-}
+# Plot
+data %>%
+  pivot_longer(c(x1, x2), names_to = "species", values_to = "x") %>%
+  ggplot(aes(x = time, y = x, group = species)) +
+  geom_line() +
+  xlab("Time (generations)") +
+  ylab("Trait value")
 
-FUN()
+
+# Initial conditions for solving the demographic equilibrium
+init <- c(1000, 1000)
+
+# Possible resident values
+xvalues <- seq(-1, 1, 0.01)
+
+# Pairwise invasibility plot (might take a while)
+pip <- plot_pip(xvalues, pars, init)
+pip2 <- plot_pip(xvalues, pars, init, binary = FALSE)
+
+# Plot the selection gradient across trait values
+tibble(
+  x = xvalues,
+  G = map_dbl(xvalues, get_gradient, pars, init)
+) %>%
+  ggplot(aes(x = x, y = G)) +
+  geom_line() +
+  geom_hline(yintercept = 0, linetype = 2) +
+  xlab(parse(text = "'Resident trait value'~hat(x)")) +
+  ylab(parse(text = "'Selection gradient'~G(hat(x))"))
+
+# Find evolutionary equilibria
+xeq <- find_singularities(from = -1, to = 1, pars, init)
+
+# Is the equilibrium stable?
+is_stable(xeq, pars, init)
+
+# Is the equilibrium attainable?
+is_convergent(xeq, pars, init)
+
+# Simulate evolution
+data <- simulate(0, 300, pars, init, burnin = 100, mu = mu, sigma = mu)
+
+data %>%
+  pivot_longer(c(x1, x2), names_to = "species", values_to = "x") %>%
+  ggplot(aes(x = time, y = x, group = species)) +
+  geom_line() +
+  xlab("Time (generations)") +
+  ylab("Trait value")
+
+
+
+
